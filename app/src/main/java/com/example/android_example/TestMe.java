f26f004577805f500f002e644d922f7d4c546679
Alex Korn <alex@iotiq.de>
2021-06-01 11:35:24 +0200

    slow (thread 500)

--- app/src/main/java/com/example/android_example/TestMe.java
+++ app/src/main/java/com/example/android_example/TestMe.java
@@ -4,6 +4,11 @@ public class TestMe {
     // will always return 1
     public int test(){
         int tmp = 2;
+        try {
+            Thread.sleep(1 * 500);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
         for (int i = 0; i < 10; i++) {
             tmp += i^i;
         }
