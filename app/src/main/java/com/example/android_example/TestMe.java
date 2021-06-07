f5b86561ede556ae285f6a1d9fc2f16e35c8251e
Alex Korn <alex@iotiq.de>
2021-06-07 16:52:45 +0200

    slow

--- app/src/main/java/com/example/android_example/TestMe.java
+++ app/src/main/java/com/example/android_example/TestMe.java
@@ -4,8 +4,8 @@ public class TestMe {
     // will always return 1
     public int test(){
         int tmp = 2;
-        // for (int i = 0; i < 90000000; i++) {
-        for (int i = 0; i < 90; i++) {
+        for (int i = 0; i < 90000000; i++) {
+        // for (int i = 0; i < 90; i++) {
             tmp += i^i;
         }
         return tmp;
